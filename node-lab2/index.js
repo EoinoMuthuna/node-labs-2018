8a0e0d7f88643f03185178fee2f0270b4c23ff64
eoinomahony <20066359@mail.wit.ie>
2018-02-14 17:03:21 +0000

    added routing for add,update,delete contact

--- node-lab2/index.js
+++ node-lab2/index.js
@@ -1,7 +1,7 @@
 import dotenv from 'dotenv';
 import express from 'express';
 import contactsRouter from './api/contacts';
-
+import bodyParser from 'body-parser';
 dotenv.config();
 
 const app = express();
@@ -9,6 +9,10 @@ const app = express();
 const port = process.env.PORT;
 
 app.use(express.static('public'));
+
+//configure body-parser
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded());
 
 app.use('/api/contacts', contactsRouter);
 app.use(express.static('public'));
